53b41969f1f4a20d79fe0d1f97c9818698c31b64
JohnPlechaty <johnplechaty@gmail.com>
2024-11-01 21:34:03 +0100

    image fuckery

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,24 @@
 // get current year
-(function () {
+/*(function () {
     var year = new Date().getFullYear();
     document.querySelector("#currentYear").innerHTML = year;
-})();
+})();*/
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log('Carousel loaded');
+    const carousel = document.getElementById("carousel-inner");
+    const firstImage = document.querySelector(".first-image");
+  
+    firstImage.onload = function() {
+      const imageHeight = firstImage.naturalHeight;
+      carousel.style.maxHeight = `500px`;
+      console.log('set ' + carousel + ' maxHeight to ' + imageHeight +' px')
+    };
+  
+    // If the image is cached and already loaded, trigger the onload manually
+    if (firstImage.complete) {
+      firstImage.onload();
+    }
+  });
+  
