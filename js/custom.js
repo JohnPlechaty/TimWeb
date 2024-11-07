3c01bb8c381b93bff0837ae28e43b47f0d3ce19f
JohnPlechaty <johnplechaty@gmail.com>
2024-11-07 22:26:15 +0100

    fixed carousel img size restrictions

--- js/custom.js
+++ js/custom.js
@@ -9,16 +9,5 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log('Carousel loaded');
     const carousel = document.getElementById("carousel-inner");
     const firstImage = document.querySelector(".first-image");
-  
-    firstImage.onload = function() {
-      const imageHeight = firstImage.naturalHeight;
-      carousel.style.maxHeight = `500px`;
-      console.log('set ' + carousel + ' maxHeight to ' + imageHeight +' px')
-    };
-  
-    // If the image is cached and already loaded, trigger the onload manually
-    if (firstImage.complete) {
-      firstImage.onload();
-    }
   });
   
